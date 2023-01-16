f2786fcdfe54302967a6afbdd467fc03f179ce2c
Katari Lokeswara rao <111894942+lokesh-katari@users.noreply.github.com>
2023-01-16 20:41:11 +0530

    added responsiveness

--- script.js
+++ script.js
@@ -25,4 +25,9 @@ function addRecommendation() {
       document.getElementById('popup').style.visibility = 'hidden'
     }
   }
+  const toggleButton = document.getElementsByClassName('toggle-button')[0]
+  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
   
+  toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+  })
